67b20924b274684c5a9e6534f3a1caff96c166dd
singhmanish03 <mailsinghmanish03@gmail.com>
2024-01-25 01:30:52 +0530

    Connect to database done

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from 'express' ;
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Connected to Db") ;
+}).catch((error) =>{
+    console.log("This is error----->" ,error);
+});
+
 const app =express() ;
+
 
 
 app.listen(3000,() => {
