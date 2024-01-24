ae22743c638ef039f0b6026c113c1afb7f2099aa
singhmanish03 <mailsinghmanish03@gmail.com>
2024-01-25 00:33:36 +0530

    Header ANd backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes,Route} from 'react-router-dom' ; 
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Home from './pages/Home';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+       <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes> 
+    </BrowserRouter>
+  )
+}
+
+export default App
