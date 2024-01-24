a13fcccb0d489a00e520204d99870fcbc312d160
singhmanish03 <mailsinghmanish03@gmail.com>
2024-01-25 02:23:57 +0530

    Test Api routes added

--- api/index.js
+++ api/index.js
@@ -1,20 +1,25 @@
-import express from 'express' ;
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import { error } from 'console';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// import { error } from 'console';
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("Connected to Db") ;
-}).catch((error) =>{
-    console.log("This is error----->" ,error);
-});
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("Connected to Db");
+    })
+    .catch((error) => {
+        console.log("This is error----->", error);
+    });
 
-const app =express() ;
+const app = express();
 
-
-
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("Server running at 3000  ddS@@@ !!");
 });
+
+//api route
+app.use("/api/user", userRouter);
