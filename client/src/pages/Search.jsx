409638d6b0260f2542431c035c55433db209d45d
singhmanish03 <mailsinghmanish03@gmail.com>
2024-01-28 15:48:24 +0530

    Create the listing item component and show all listing

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Listing from "./Listing";
+import ListingItem from "../components/ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
@@ -15,8 +17,6 @@ export default function Search() {
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
   const [showMore, setShowMore] = useState(false);
-
-  
 
   //use effect isliye use kiye kuki jab search kare toh rul me change ho aur searhc me khud se likha jaye
   useEffect(() => {
@@ -228,10 +228,21 @@ export default function Search() {
 
       {/* right side */}
 
-      <div className="">
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
-          Listing Results :{" "}
+          Listing Results :
         </h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700 ">No Listing Found!!</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">
+              Loading....
+            </p>
+          )}
+          {!loading && listings && listings.map((listing) => <ListingItem key={listing._id} listing={listing}/>)}
+        </div>
       </div>
     </div>
   );
